e29a3eb7d76e83106964f9530a9e230df0fe4b21
罗德 <1846555387@qq.com>
2024-09-12 00:02:03 +0800

    feat: 透明代理

---
+++ 48.透明代理/contracts/Logic1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// wtf.academy
+pragma solidity ^0.8.21;
+
+// 逻辑合约1
+contract Logic1 {
+    // 状态变量和proxy合约一致，防止插槽冲突
+    address public implementation;
+    address public admin;
+    string public words; // 字符串，可以通过逻辑合约的函数改变
+
+    // 改变proxy中状态变量，选择器： 0xc2985578
+    function foo() public {
+        words = "old";
+    }
+}
